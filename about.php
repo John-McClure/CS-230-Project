0e144ea575eafe1f6aa28100a32aae56e68f5452
John McClure <jmmcclure014@gmail.com>
2020-09-25 19:22:25 -0400

    Lab 4. implemented login and logout capabilities

---
+++ about.php
@@ -0,0 +1,56 @@
+<?php
+require 'includes/header.php'
+?>
+<main>
+<link rel="stylesheet" href="css/about.css">
+    <div class="about">
+        <h1>About Us</h1>
+        <p>Hello! Meet the team behind the number one video game review company! We are extremely passionate about our
+            work, and each of us work tirelessly to bring you up to date reviews on the newest video games.</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/Me.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>John McClure</p>
+                    <p>Email: jmm0155@mix.wvu.edu</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/egordon.jpg">
+                <div class="container">
+                    <h2>COO</h2>
+                    <p>E. Gordon Gee</p>
+                    <p>Email: presidentGee@mail.wvu.edu</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/george.jpg">
+                <div class="container">
+                    <h2>Lead 
+                      Designer</h2>
+                    <p>George Washington</p>
+                    <p>Email: GWashington@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/kratos.png">
+                <div class="container">
+                    <h2>Art Director</h2>
+                    <p>Kratos</p>
+                    <p>Email: kratos101@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</main>
