3296922021d2edbc37c7f2b296b5d1d8972ca95a
John McClure <jmmcclure014@gmail.com>
2020-10-09 21:05:21 -0400

    Lab 5. Profile Pic Upload

--- includes/signup-helper.php
+++ includes/signup-helper.php
@@ -45,7 +45,10 @@ if(isset($_POST['signup-submit'])){
                     mysqli_stmt_execute($stmt);
                     mysqli_stmt_store_result($stmt);
 
-                    header("Location ../signup.php?signup=success");
+                    $sqlImg = "INSERT INTO profile (uname) VALUES ('$username')";
+                    mysqli_query($conn, $sqlImg);
+
+                    header("Location: ../signup.php?signup=success");
                     exit();
                 }
             }
